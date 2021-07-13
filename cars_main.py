0849c30939f9f7e6ee652de69ac8a7304afdeac0
Alexander Justin King Sing Ortiz <ajkso92@gmail.com>
2021-07-12 23:06:31 -0400

    initial main initialization

--- cars_main.py
+++ cars_main.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import streamlit as st
 
-st.write("TESTOO")
+from tensorflow.keras.models import load_model
+from joblib import load, dump
+
+model = load_model("Keras Model")
+
+categorical_encoder = load("categorical_encoder.pkl")
+numerical_encoder = load("numerical_encoder.pkl")
+
+## dimensional characteristics -- > 11 dimensions
+
+st.write(categorical_encoder.get_feature_names())  ### GET THE MANUFACTURERS AND MODELS FROM THE TABLES INSTEAD OF THE ENCODERS.
