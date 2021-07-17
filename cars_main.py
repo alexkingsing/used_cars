2216f33d8775064b981796e2779d42b94ecf8352
Alexander Justin King Sing Ortiz <ajkso92@gmail.com>
2021-07-16 23:16:42 -0400

    initializing app development

--- cars_main.py
+++ cars_main.py
@@ -1,16 +1,45 @@
-import pandas as pd
 import streamlit as st
+from utils import *
+from tensorflow.python.keras.saving.save import load_model
 
-from tensorflow.keras.models import load_model
-from joblib import load, dump
+#configs
+st.set_page_config(layout="wide")
 
+# Start
+st.title("HERE COMES THE BOOM")
+opt = st.sidebar.radio("Let's go!", ["Introduction","Price prediction"], )
+
+# Loading model, categories and encoders.
 model = load_model("Keras Model")
+categorical_encoder, numerical_encoder = load_encoders()
+base = load_categories()
 
-categorical_encoder = load("categorical_encoder.pkl")
-numerical_encoder = load("numerical_encoder.pkl")
+if opt == "Introduction":
 
-## dimensional characteristics -- > 11 dimensions
+    '''
+    STEP 1:
+    * Create a small introduction page explaining what the project is, where it came from (in summary) and describe what the project
+    is going to do.
 
-st.write(categorical_encoder.get_feature_names())  ### GET THE MANUFACTURERS AND MODELS FROM THE TABLES INSTEAD OF THE ENCODERS.
+    STEP 2: 
+    * Create supporting functions so that the front-end side of the application is not loaded with too many options.
+    * Cache as many things as possible.
 
-### EXPORT THE TABLE OF NUNIQUE? OR PERFORM MANUFACTURER-WISE QUERYING.
+    STEP 3:
+    * Decide if using a FORM or using SIDEBAR is more intuitive.
+    * Develop the selection side of the app
+
+    STEP 4:
+    * Retrieve the relevant categorical / numerical information from the FORM/SIDEBAR
+    * Encoder the information and perform prediction test
+
+    STEP 5:
+    * Create a plot showing the exact prediction and a range of MAX/MIN based on the models MAE.
+
+    OPT STEP 6:
+    * Look for an image of the vehicle and include it as part of the results.
+
+    '''
+
+else:
+    st.table(base.head())
---
+++ utils.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+from joblib import load
+
+
+## COMPLETED
+@st.cache  # caching all the loaders to avoid overhead lag
+def load_encoders() -> tuple:
+    '''
+    A simple function to mask and simplify the loading of both encoders.
+    
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    A tuple of encoders 
+    '''
+
+    categorical_encoder = load("categorical_encoder.pkl")
+    numerical_encoder = load("numerical_encoder.pkl")
+
+    return categorical_encoder, numerical_encoder
+
+## COMPLETED
+@st.cache  # caching all the loaders to avoid overhead lag
+def load_categories() -> pd.DataFrame:
+    '''
+    This function loads the dataframe that contains the labels used during training of the model.
+    All manufacturers and models not included in this dataframe are not supported by this project.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    A tuple of encoders
+    '''
+    sample_base = load("categories_file.pkl")
+
+    return sample_base
+
+def transform(data, cat_enc, num_enc) -> np.array:
+    '''
+    Function to transform input data into a prediction-ready array.
+
+    Parameters
+    ----------
+    data: Iterable, numpy array or Pandas Dataframe holding the data needed for prediction.
+    cat_enc: Pre-fit categorical encoder to transform the categorical features into integers via one-hot encoding.
+    num_enc: Pre-fit encoder to transform numerical features into a scaled form.
+
+    Returns
+    -------
+    a numpy array with the transformed array of data, prediction-ready.
+    '''
+    pass
+
+def plot():
+    pass
